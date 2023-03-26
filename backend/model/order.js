9a7ded2cc47ea599d2dcc6ba0129de34c077845f
Rajit Kumar <krajit909@gmail.com>
2023-03-26 16:41:49 +0530

    Orders model completed

--- backend/model/order.js
+++ backend/model/order.js
@@ -1,7 +1,35 @@
 import mongoose from "mongoose";
 
+const {ObjectId} = mongoose.Schema;
+
+const ProductCartSchema = new mongoose.Schema({
+    product: {
+        type: ObjectId,
+        ref: "Product"
+    },
+    name: String,
+    count: Number,
+    price: Number
+})
+
+const ProductCart = mongoose.model("ProductCart", ProductCartSchema)
+
 const orderSchema = new mongoose.Schema(
     {
-        
-    }
-)
+        product: [ProductCartSchema],
+        transaction_id: {},
+        amount: {
+            type: Number
+        },
+        address: String,
+        updated: Date,
+        user: {
+            type: ObjectId,
+            ref: " User"
+        }
+    },
+    {timestamps: true}
+);
+
+const Order = mongoose.model("Order", orderSchema);
+export default {ProductCart, Order};
