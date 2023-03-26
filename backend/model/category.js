296a56b32683b078f52c1adfbd62e92ef222c43e
Rajit Kumar <krajit909@gmail.com>
2023-03-26 16:11:26 +0530

    category model completed

--- backend/model/category.js
+++ backend/model/category.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-        maxlength: 25
-    }
-})
+const categorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32,
+            unique: true
+        }
+    },
+    {timestamps: true}
+)
+
+
+const CategoryModel = mongoose.model("Category", categorySchema);
+export default CategoryModel; 
