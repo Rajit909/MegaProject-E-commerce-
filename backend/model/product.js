40d57ae3a3afe1802cc6c44afd93b9bcec5d3739
Rajit Kumar <krajit909@gmail.com>
2023-03-26 16:24:06 +0530

    product model

--- backend/model/product.js
+++ backend/model/product.js
@@ -1,7 +1,49 @@
 import mongoose from "mongoose";
 
+const {ObjectId} = mongoose.Schema;
+
 const productSchema = new mongoose.Schema(
     {
-        
-    }
-)
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32,
+        },
+        description: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 2000,
+        },
+        price: {
+            type: Number,
+            trim: true,
+            required: true,
+            maxlength: 32,
+        },
+        category: {
+            type: ObjectId,
+            ref: "Category",
+            required: true,
+            // trim: true,
+            // maxlength: 32,
+        },
+        stock: {
+            type: Number,
+
+        },
+        sold: {
+            type: Number,
+            default: 0
+        },
+        photo: {
+            data: Buffer,
+            contentType: String
+        }
+    },
+    {timestamps: true}
+);
+
+const ProductModel = mongoose.model("Product", productSchema);
+export default ProductModel;
