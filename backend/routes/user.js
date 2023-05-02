f79d98043a6069dc50e33ad90c97f0b1ef63f5aa
Rajit Kumar <krajit909@gmail.com>
2023-05-02 22:12:05 +0530

    bug fixed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router()
 import {getUserById, getUser} from "../Controller/user.js"
 import {isSignedIn, isAuthenticated, isAdmin} from "../Controller/auth.js"
 
+
 router.param("userId", getUserById);
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
