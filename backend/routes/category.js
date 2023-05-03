cd15555231d38edcd2e11eaa3805c70969bb91c1
Rajit Kumar <krajit909@gmail.com>
2023-05-03 15:53:48 +0530

    category router created

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,8 +1,12 @@
 import express  from "express"
 const router = express.Router()
 
+
+import { isAdmin, isAuthenticated, isSignedIn } from "../Controller/auth"
 import { getUserById } from "../Controller/user"
 
+
+router.param("userId", getUserById);
 
 
 export default router
