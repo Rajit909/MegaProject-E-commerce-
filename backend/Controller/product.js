203cba240dec998204812494f9136a549efd2c31
Rajit Kumar <krajit909@gmail.com>
2023-05-04 21:52:19 +0530

    saving and uploading file in create product method

--- backend/Controller/product.js
+++ backend/Controller/product.js
@@ -1,5 +1,7 @@
 import Product from "../models/product.js"
 import formidable from "formidable"
+import _ from "lodash"
+import fs from "fs"
 
 const getProductById = (req, res, next, id) => {
     Product.findById(id)
@@ -16,7 +18,40 @@ const getProductById = (req, res, next, id) => {
 }
 
 const createProduct = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
 
+    form.parse(req, (err, fields, file) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Problem with image"
+            })
+        }
+
+        // restriction on fields
+        let product = new Product(fields)
+
+        // handle file
+        if (file.photo) {
+            if (file.photo.size > 3000000) {
+                return res.status(400).json({
+                    error: "File size too big"
+                })
+            }
+            product.photo.data = fs.readFileSync(file.photo.path)
+            product.photo.contentType = file.photo.type
+        }
+
+        // save to the DB
+        product.save((err, product) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "saving product in DB"
+                })
+            }
+            res.json(product)
+        })
+    })
 }
 
 export {
