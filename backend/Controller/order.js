f5ca5f954022202a640da7572a17fd99567ae42f
Rajit Kumar <krajit909@gmail.com>
2023-05-05 14:40:29 +0530

    createOrder method

--- backend/Controller/order.js
+++ backend/Controller/order.js
@@ -1,11 +1,36 @@
-import {Order} from "../models/order.js";
+import orderModel from "../models/order.js";
 
+const {Order, ProductCart} = orderModel;
 
 const getOrderById = (req, res, next, id) => {
+    Order.findById(id)
+    .populate("products.product", "name price")
+    .exec((err, order) => {
+        if(err){
+            return res.status(400).json({
+                error: 'No Order found in DB'
+            })
+        }
+        req.order = order
+        next();
+    })
+};
 
+
+const createOrder = (req, res) => {
+    req.body.order = req.profile
+    const order = new Order(req.body.order)
+    order.save((err, order) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Failed to save order in DB"
+            })
+        }
+        res.json(order)
+    })
 }
-
 
 export {
-    getOrderById
-}
+  getOrderById,
+  createOrder
+};
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -3,15 +3,24 @@ const router = express.Router();
 
 import { isSignedIn, isAuthenticated, isAdmin } from "../Controller/auth.js";
 import { getUserById, pushOrderInPurchageList } from "../Controller/user.js";
-import {updateStock} from "../Controller/product.js"
+import { updateStock } from "../Controller/product.js";
 
-import{
-    getOrderById
-}
-from "../Controller/order.js";
+import { getOrderById, createOrder } from "../Controller/order.js";
 
 // params
 router.param("userId", getUserById);
 router.param("orderId", getOrderById);
 
-export default router
+// actual
+// create
+router.post(
+  "/order/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  pushOrderInPurchageList,
+  updateStock,
+  createOrder
+);
+// read
+
+export default router;
