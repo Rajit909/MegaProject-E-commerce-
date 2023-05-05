88d8c84c138b5b2a33a7ca7504ad1be2bb540115
Rajit Kumar <krajit909@gmail.com>
2023-05-05 13:28:56 +0530

    updateStock middleware

--- backend/Controller/product.js
+++ backend/Controller/product.js
@@ -151,6 +151,26 @@ const getAllProducts = (req, res) => {
     });
 };
 
+const updateStock = (req, res, next) => {
+    let myOperations = req.body.order.products.map(prod => {
+        return {
+            updateOne: {
+                filter: {_id: prod._id},
+                update: {$inc: {stock: -prod.count, sold: +prod.count}}
+            }
+        }
+    })
+
+    Product.bulkWrite(myOperations, {}, (err, products) => {
+        if(err){
+            return res.status(400).json({
+                error: "Bulk Operations failed"
+            })
+        }
+        next();
+    })
+}
+
 export {
   getProductById,
   createProduct,
