feb107115208c32998503c17ba18a41bc78d8a10
Rajit Kumar <krajit909@gmail.com>
2023-05-05 08:56:56 +0530

    bug fixed

--- backend/Controller/product.js
+++ backend/Controller/product.js
@@ -1,75 +1,67 @@
-import Product from "../models/product.js"
-import formidable from "formidable"
-import _ from "lodash"
-import fs from "fs"
+import Product from "../models/product.js";
+import formidable from "formidable";
+import _ from "lodash";
+import fs from "fs";
 
 const getProductById = (req, res, next, id) => {
-    Product.findById(id)
+  Product.findById(id)
     .populate("category")
     .exec((err, product) => {
-        if (err) {
-            return res.status(400).json({
-                error: "Product not Found"
-            })
-        }
-        req.product = product;
-        next();
-    })
-}
+      if (err) {
+        return res.status(400).json({
+          error: "Product not Found",
+        });
+      }
+      req.product = product;
+      next();
+    });
+};
 
 const createProduct = (req, res) => {
-    let form = new formidable.IncomingForm();
-    form.keepExtensions = true;
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
 
-    form.parse(req, (err, fields, file) => {
-        if (err) {
-            return res.status(400).json({
-                error: "Problem with image"
-            })
-        }
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Problem with image",
+      });
+    }
 
-        // destructing the fields
-        const {name, description, price,category, stock} = fields;
+    // destructing the fields
+    const { name, description, price, category, stock } = fields;
 
-        if (
-            !name ||
-            !description ||
-            !price ||
-            !category ||
-            !stock 
-            ){
-            return res.status(400).json({
-                error: "All fields are required!, Please fill All fields"
-            })
-        }
+    if (!name || !description || !price || !category || !stock) {
+      return res.status(400).json({
+        error: "All fields are required!, Please fill All fields",
+      });
+    }
 
-        // restriction on fields
-        let product = new Product(fields)
+    // restriction on fields
+    let product = new Product(fields);
 
-        // handle file
-        if (file.photo) {
-            if (file.photo.size > 3000000) {
-                return res.status(400).json({
-                    error: "File size too big"
-                })
-            }
-            product.photo.data = fs.readFileSync(file.photo.path)
-            product.photo.contentType = file.photo.type
-        }
+    // handle file
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size too big",
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.filepath);
+      product.photo.contentType = file.photo.type;
+    }
 
-        // save to the DB
-        product.save((err, product) => {
-            if (err) {
-                return res.status(400).json({
-                    error: "Not able save product in DB"
-                })
-            }
-            res.json(product)
-        })
-    })
-}
+    // save to the DB
+    console.log(product);
+    product.save((err, product) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Not able save product in DB",
+        });
+      }
+      res.json(product);
+    });
+  });
+};
 
-export {
-    getProductById,
-    createProduct
-}
+export { getProductById, createProduct };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,21 +1,21 @@
 import express from "express";
 const router = express.Router();
 
-import {
-    getProductById,
-    createProduct
-} from "../Controller/product.js"
+import { getProductById, createProduct } from "../Controller/product.js";
 import { isSignedIn, isAuthenticated, isAdmin } from "../Controller/auth.js";
 import { getUserById } from "../Controller/user.js";
-
 
 // params
 router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 // router
-router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct)
+router.post(
+  "/product/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
 
-
-
-export default router
+export default router;
