fa31b1bb2d84f5b8b604d7ea381870772885495d
Rajit Kumar <krajit909@gmail.com>
2023-06-20 20:36:23 +0530

    admin dashbord create category

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import Home from './core/Home.js';
-import Signup from './user/Signup.js';
-import Signin from './user/Signin.js';
+import Home from "./core/Home.js";
+import Signup from "./user/Signup.js";
+import Signin from "./user/Signin.js";
 
-import UserDashBoard from './user/UserDashBoard';
-import AdminDashBoard from './user/AdminDashBoard';
+import UserDashBoard from "./user/UserDashBoard";
+import AdminDashBoard from "./user/AdminDashBoard";
 
-import { isAuthenticated } from './auth/helper/index';
-
+import { isAuthenticated } from "./auth/helper/index";
+import AddCategory from "./admin/AddCategory.js";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/signup" exact element={<Signup />} />
-      <Route path="/signin" exact element={<Signin />} />
-      <Route exact path='/user/dashBoard' element = { isAuthenticated ? <UserDashBoard/> : <Navigate to="/signin" />} />
-      <Route exact path='/admin/dashBoard' element = { isAuthenticated ? <AdminDashBoard/> : <Navigate to="/signin" />} />
-      
-
+      <Route
+        exact 
+        path="/" 
+        element={<Home />} />
+      <Route
+        exact 
+        path="/signup" 
+        element={<Signup />} />
+      <Route
+        exact 
+        path="/signin" 
+        element={<Signin />} />
+      <Route
+        exact
+        path="/user/dashBoard"
+        element={
+          isAuthenticated ? <UserDashBoard /> : <Navigate to="/signin" />
+        }
+      />
+      <Route
+        exact
+        path="/admin/dashBoard"
+        element={
+          isAuthenticated ? <AdminDashBoard /> : <Navigate to="/signin" />
+        }
+      />
+      <Route
+        path="/admin/create/category"
+        exact
+        element={isAuthenticated ? <AddCategory /> : <Navigate to="/signin" />}
+      />
     </Routes>
   );
 }
